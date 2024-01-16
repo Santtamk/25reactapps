93161ff9b003700cfa05550ba0df0c5443f43900
Santam Kumai <santtamk@gmail.com>
2024-01-16 22:43:19 +0530

    added style-star component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@
 import './App.css';
 import Accordion from './Components/Accordian';
 import RandomColor from './Components/Random-color';
+import StarRating from './Components/star-rating';
 
 function App() {
   return (
     <div className="App">
-      {/* Accordian component */}
+
+      {/* Accordian component
       <Accordion />
 
       {/* Random Color Component */}
-      <RandomColor />
+      {/* <RandomColor /> */}
+
+      {/* Star-rating Component */}
+      <StarRating  noOfStars={8}/>
+
     </div>
   );
 }
